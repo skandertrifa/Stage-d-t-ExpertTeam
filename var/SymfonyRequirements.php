9e1583b253b05c2b706b39bb7fbfdfce382e27ee
Skander Trifa <trifa.skander@insat.u-carthage.tn>
2019-07-16 17:01:36 +0100

    new project

--- var/SymfonyRequirements.php
+++ var/SymfonyRequirements.php
@@ -594,7 +594,7 @@ class SymfonyRequirements extends RequirementCollection
 
         $this->addRecommendation(
             version_compare($installedPhpVersion, '5.4.11', '>='),
-            'When using the logout handler from the Symfony Security Component, you should have at least PHP 5.4.11 due to PHP bug #63379 (as a workaround, you can also set invalidate_session to false in the security logout handler configuration)',
+            'When using the logout handler from the Symfony Security Component, you should have at least PHP 5.4.11 due to PHP bug #63379 (as a workaround, you can also set invalidate_session to false in the Security logout handler configuration)',
             'Install PHP 5.4.11 or newer if your project uses the logout handler from the Symfony Security Component.'
         );
 
